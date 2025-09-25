99f1fd4d00f8e86a9e9f42d7d33220dd51956c6b
bear08 <957011893@qq.com>
2025-09-25 19:25:15 +0800

    update lock guard

--- cs/languages/cpp/std/thread/main.cpp
+++ cs/languages/cpp/std/thread/main.cpp
@@ -10,7 +10,6 @@ lock_guard<mutex> *lck = nullptr;
 void func1()
 {
     lck = new lock_guard<mutex>(mtx);
-    // lock_guard<mutex> lck(mtx);
     for (int i = 0; i < 1000; ++i)
         cout << i << endl;
     delete lck;
@@ -21,7 +20,6 @@ void func1()
 void func2()
 {
     lck = new lock_guard<mutex>(mtx);
-    // lock_guard<mutex> lck(mtx);
     for (int i = 0; i < 1000; ++i)
         cout << "abc" << endl;
     delete lck;
@@ -29,20 +27,11 @@ void func2()
 
 int main()
 {
-    cout << "abc" << endl;
+    // snippet lock by lock_guard
     thread t1(&func1);
     thread t2(&func2);
 
     t1.join();
     t2.join();
-    // t1.detach();
     cout << "finished" << endl;
-
-    set<int> s;
-    s.insert(1);
-    s.insert(2);
-    cout << "erase multiple times" << endl;
-    s.erase(2);
-    s.erase(2);
-    s.erase(2);
 }
